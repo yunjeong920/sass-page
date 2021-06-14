98ebf7607f2304065b0812650eb0636c670aba12
yunjeong <yunjeong920@naver.com>
2021-06-14 15:43:41 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -7,3 +7,15 @@ $(function(){
     $("#"+tab_id).addClass("current");
   })
 })
+
+$(function(){
+  $(window).scroll(function(){
+    var scroll = $(this).scrollTop();
+    if(scroll  > 1){
+      $("#header").css({"background":"rgba(255,255,255,0.9)"},{"transition":"0.3s"});
+    }
+    else{
+      $("#header").css({"background":"none"},{"transition":"0.3s"});
+    }
+  })
+})
